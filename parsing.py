067c2798a0cc97bec992800e5d351cb7d535c698
nosnawsm <jador8@gmail.com>
2014-11-25 14:36:01 -0800

    Added first code barerror plots

--- parsing.py
+++ parsing.py
@@ -1,6 +1,45 @@
 import csv;
-#open the datafile
+import numpy as np;
+import datetime;
+import matplotlib.pyplot as plt;
+#open the datafile and plotting tools
 header=[];
+
+def parse_time(date):
+    if( '-' in date):
+        return '';
+    lst=date.split('/');
+    ret_lst=[];
+    print lst;
+    for itm in lst:
+        print itm;
+        ret_lst.append( int(itm) );
+    return datetime.datetime(year=2000+ret_lst[2], month=ret_lst[0], day=ret_lst[1]);
+    
+def create_barerror_plot(csv_file):
+    lst=[];
+    name_lst=[];
+    date_lst=[];
+    with open('NonFukushima.csv','rU') as csvfile:
+        #read the first line
+        parser=csv.reader(csvfile);
+        header=parser.next();
+        #use the first line to create a dictionary to get values easily
+        dictparser=csv.DictReader(csvfile,header);
+        for row in dictparser:
+            tmp_list=[];
+            name_lst.append(row[header[0]]);
+            date_lst.append(parse_time(row[header[1]]));
+            for ind in range(2,len(row)):
+                tmp_list.append(float(row[header[ind]]));
+            lst.append(tmp_list);
+            
+    #change in the datatype for conv.
+    lst=np.asarray(lst);
+    print lst,name_lst,date_lst;
+
+
+create_barerror_plot('NonFukushima.csv');
 with open('NonFukushima.csv','rU') as csvfile:
      #read the first line
      parser=csv.reader(csvfile);
