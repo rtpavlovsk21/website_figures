39a4f18bb9a4a8a185964d61be97b00825fda86e
pavlovsr-chrome <rtpavlovsk21@gmail.com>
2014-11-25 17:38:55 -0800

    testing the generation

--- parsing.py
+++ parsing.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt;
 #open the datafile and plotting tools
 header=[];
 #number of collumns containing non-data -1
-metacols = 2;
+metacols = 3;
 
 def parse_time(date):
     if( '-' in date):
@@ -20,6 +20,7 @@ def create_barerror_plot(csv_file):
     lst=[];
     name_lst=[];
     date_lst=[];
+    header=[];
     with open('NonFukushima.csv','rU') as csvfile:
         #read the first line
         parser=csv.reader(csvfile);
@@ -36,8 +37,37 @@ def create_barerror_plot(csv_file):
             
     #change in the datatype for conv.
     lst=np.asarray(lst);
-    print lst,name_lst,date_lst;
     
+    #making labels for the chart
+    for itm in range(0,len(name_lst)):
+        name_lst[itm]=name_lst[itm]+"\n"+str(date_lst[itm]);
+    data =np.zeros((len(name_lst),lst.shape[1]+2-metacols));
+    error=np.zeros((len(name_lst),lst.shape[1]+2-metacols));
+    legend_key=[];
+    for itm in range(0,lst.shape[1],2):
+        legend_key.append(header[metacols+itm]);
+        data[:,itm]=lst[:,itm];
+        error[:,itm]=lst[:,itm+1];
+    print legend_key;
+    ax,fig=generate_barerror_logy(sample_names=name_lst,data=data,error=error,legend_key=legend_key,log=True);
+
+def generate_barerror_logy(sample_names,data,error,legend_key,log=True):
+    num_samples=len(sample_names);
+    
+    ind = np.arange(num_samples);
+    width=0.35;
+
+    fig,ax=plt.subplots();
+    
+    axs=[];
+    for samp in range(0,len(sample_names)):
+        axs.append(left=ax.bar(ind+width*samp,height=data[ind,:],width=width,color='r',yerr=error[ind,:],log=True));
+
+    ax.set_xticks( ind+width );
+    ax.set_xticklabels( sample_names );
+    ax.legend( [axs[:][0]], sample_names);
+    plt.show();
+    return ax,fig;
 
 create_barerror_plot('NonFukushima.csv');
 with open('NonFukushima.csv','rU') as csvfile:
@@ -68,7 +98,7 @@ with open('jquery.header') as header_file:
      val=val[:-1];
      for line in header_file:
           val+=line;
-print val;
+#print val;
      
 with open('Data.js','w') as data:
      data.write(val_pre+val)
