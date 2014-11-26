d65e986ff72a3648f058b9702ccb36eb8e1d86ee
etci <etci@etci.(none)>
2014-11-25 19:08:59 -0800

    adding colr_scheme and other plotting utils

--- parsing.py
+++ parsing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt;
 header=[];
 #number of collumns containing non-data -1
 metacols = 3;
-
+colr_scheme=['#00B2A5','#D9661F','#00B0DA','#FDB515','#ED4E33','#2D637F','#9DAD33','#53626F','#EE1F60','#6C3302','#C2B9A7','#CFDD45','#003262'];
 def parse_time(date):
     if( '-' in date):
         return '';
@@ -37,35 +37,40 @@ def create_barerror_plot(csv_file):
             
     #change in the datatype for conv.
     lst=np.asarray(lst);
-    
+    print lst;
     #making labels for the chart
     for itm in range(0,len(name_lst)):
         name_lst[itm]=name_lst[itm]+"\n"+str(date_lst[itm]);
-    data =np.zeros((len(name_lst),lst.shape[1]+2-metacols));
-    error=np.zeros((len(name_lst),lst.shape[1]+2-metacols));
+    data =np.zeros((len(name_lst),lst.shape[1]/2.));
+    error=np.zeros((len(name_lst),lst.shape[1]/2.));
     legend_key=[];
+    loop=0;
     for itm in range(0,lst.shape[1],2):
         legend_key.append(header[metacols+itm]);
-        data[:,itm]=lst[:,itm];
-        error[:,itm]=lst[:,itm+1];
+        data[:,loop]=lst[:,itm];
+        error[:,loop]=lst[:,itm+1];
+        loop+=1;
     print legend_key;
+    print data.shape, error.shape;
     ax,fig=generate_barerror_logy(sample_names=name_lst,data=data,error=error,legend_key=legend_key,log=True);
 
 def generate_barerror_logy(sample_names,data,error,legend_key,log=True):
     num_samples=len(sample_names);
     
     ind = np.arange(num_samples);
-    width=0.35;
+    width=0.15;
 
     fig,ax=plt.subplots();
     
     axs=[];
-    for samp in range(0,len(sample_names)):
-        axs.append(left=ax.bar(ind+width*samp,height=data[ind,:],width=width,color='r',yerr=error[ind,:],log=True));
-
-    ax.set_xticks( ind+width );
-    ax.set_xticklabels( sample_names );
-    ax.legend( [axs[:][0]], sample_names);
+    for samp in range(0,len(legend_key)):
+        if( np.amin(data[:,samp])==0):
+            data[:,samp]+=np.amin(error[:,samp])/5;    
+        axs.append(ax.bar(left=ind+width*samp,height=tuple(data[:,samp]),width=width,color=colr_scheme[samp],yerr=tuple(error[:,samp]),log=True));
+    
+    #ax.set_xticks( ind+width );
+    #ax.set_xticklabels( sample_names );
+    #ax.legend( [axs[:][0]], sample_names);
     plt.show();
     return ax,fig;
 
